67c19da0f63e324a5a32c37d41a99d732842bd86
ililis <47765849+ililis@users.noreply.github.com>
2022-11-24 15:54:37 +0700

    1 file modified, 2 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,13 +16,19 @@ export class HeroesComponent {
 
   ngOnInit(): void {
     // this.hero.name = "lalala"
+    this.getHeroes();
     console.log(this.singlehero.name);
   }
   ngAfterContentChecked(): void {
     // console.log(this.singlehero.name);
   }
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
