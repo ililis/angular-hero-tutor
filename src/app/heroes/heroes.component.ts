8f5c29201447a484685d83d3c8964cb8a0b93584
ililis <47765849+ililis@users.noreply.github.com>
2022-11-24 16:58:41 +0700

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -19,20 +20,28 @@ export class HeroesComponent {
     this.getHeroes();
     console.log(this.singlehero.name);
   }
-  ngAfterContentChecked(): void {
-    // console.log(this.singlehero.name);
-  }
-  constructor(private heroService: HeroService) {}
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
+
+  selectedHero?: Hero;
 
   // heroes = HEROES;
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  ngAfterContentChecked(): void {
+    // console.log(this.singlehero.name);
+  }
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     console.log(hero.name);
   }
 }
