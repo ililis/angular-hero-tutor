17250c7448f04abca911bbec1527dfa67c2b0fd6
ililis <47765849+ililis@users.noreply.github.com>
2022-11-24 12:56:56 +0700

    4 files modified, 1 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
+  singlehero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
   };
+  // nothero = 'what??';
+
+  ngOnInit(): void {
+    // this.hero.name = "lalala"
+    console.log(this.singlehero.name);
+  }
+  ngAfterContentChecked(): void {
+    // console.log(this.singlehero.name);
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(hero.name);
+  }
 }
