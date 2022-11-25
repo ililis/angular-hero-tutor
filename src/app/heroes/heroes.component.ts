db400637dca24010bef720d2a1c2ce6d2d150f51
ililis <47765849+ililis@users.noreply.github.com>
2022-11-25 11:26:09 +0700

    7 files modified, 4 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,39 +8,15 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  singlehero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  // nothero = 'what??';
-
-  ngOnInit(): void {
-    // this.hero.name = "lalala"
-    this.getHeroes();
-    console.log(this.singlehero.name);
-  }
-
-  selectedHero?: Hero;
-
-  // heroes = HEROES;
   heroes: Hero[] = [];
 
-  ngAfterContentChecked(): void {
-    // console.log(this.singlehero.name);
-  }
+  constructor(private heroService: HeroService) {}
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-    console.log(hero.name);
   }
 }
